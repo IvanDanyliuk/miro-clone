405350ab203aefe736ea9c494577cbc129b66a80
IvanDanyliuk <ivandaniliuk@gmail.com>
2024-07-10 18:37:50 +0300

    implement room info

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -9,10 +9,12 @@ import { Button } from '@/components/ui/button';
 import { LayoutDashboard, Star } from 'lucide-react';
 import { useSearchParams } from 'next/navigation';
 
+
 const font = Poppins({
   subsets: ['latin'],
   weight: ['600']
 });
+
 
 export const OrgSidebar = () => {
   const searchParams = useSearchParams();
--- app/board/[boardId]/_components/loading.tsx
+++ app/board/[boardId]/_components/loading.tsx
@@ -1,16 +1,16 @@
 import { Loader } from 'lucide-react';
-import { Info } from './info';
-import { Participants } from './participants';
-import { Toolbar } from './toolbar';
+import { InfoSkeleton } from './info';
+import { ParticipantsSkeleton } from './participants';
+import { ToolbarSkeleton } from './toolbar';
 
 
 export const Loading = () => {
   return (
     <main className='w-full h-full relative flex justify-center items-center bg-neutral-100 touch-none'>
       <Loader className='w-6 h-6 text-muted-foreground animate-spin' />
-      <Info.Skeleton />
-      <Participants.Skeleton />
-      <Toolbar.Skeleton />
+      <InfoSkeleton />
+      <ParticipantsSkeleton />
+      <ToolbarSkeleton />
     </main>
   );
 };
